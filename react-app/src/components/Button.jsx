2041101ab17edb6ef1f025012d8b8111bbb63058
Hayoon Cho <117485593+laurenCho9@users.noreply.github.com>
2023-04-23 00:00:47 +0900

    경로 수정

--- react-app/src/components/Button.jsx
+++ react-app/src/components/Button.jsx
@@ -4,7 +4,7 @@ import styles from "../Button.module.css";
 function Button({ text }) {
   return (
     <>
-      <button className={styles.btn}>{text}</button>;
+      <button className={styles.btn}>{text}</button>
     </>
   );
 }
