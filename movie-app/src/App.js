eb3270bc9e343aba7bf82d7fef799f5a3b012af1
Hayoon Cho <117485593+laurenCho9@users.noreply.github.com>
2023-05-23 23:58:40 +0900

    Add: styled-components

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,18 +1,31 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./components/routes/Home";
 import Detail from "./components/routes/Detail";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div>
+    <Container>
       <Router>
         <Routes>
           <Route path="/movie/:id" element={<Detail />} />
           <Route path="/*" element={<Home />} />
         </Routes>
       </Router>
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  li {
+    list-style: none;
+  }
+`;
 
 export default App;
